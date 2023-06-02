e2c791858434ed5418c84ae08b6e06900d15ffe5
jasonpellegrini <jpellegrini1616@gmail.com>
2023-06-02 18:08:59 -0400

    Started implementing geocoding

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,14 +1,34 @@
 import React from "react"
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker } from 'react-leaflet'
 import "leaflet/dist/leaflet.css"
 import './browse.css'
+import axios from 'axios';
 
 export default function Browse() {
+
+    const getCoordinates = async () => {
+        const address = "3720 Dawson Street"
+
+        try {
+            const url = `https://nominatim.openstreetmap.org/search?q=${address}&limit=2&format=json`;
+            const response = await axios.get(url);
+            console.log("response:", response);
+        } catch {
+            console.log("error")
+        }
+    }
+    
+
+
     return (
-        <MapContainer center={[40.431988,-79.959838]} zoom={13}>
-            <TileLayer 
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-        </MapContainer>
+        <div>
+            <button onClick={getCoordinates}>Button</button>
+        
+            <MapContainer center={[40.431988,-79.959838]} zoom={15}>
+                <TileLayer 
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+            </MapContainer>
+        </div>
     )
 }
