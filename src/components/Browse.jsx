a9b6cdc56786544e559ed1db93bef52b68fcd9e1
jasonpellegrini <jpellegrini1616@gmail.com>
2023-06-01 23:43:17 -0400

    Added map component with leaflet js

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,9 +1,14 @@
 import React from "react"
+import { MapContainer, TileLayer } from 'react-leaflet'
+import "leaflet/dist/leaflet.css"
+import './browse.css'
 
 export default function Browse() {
     return (
-        <div>
-           <p>This is the page where the user will be able to browse through the map.</p> 
-        </div>
+        <MapContainer center={[40.431988,-79.959838]} zoom={13}>
+            <TileLayer 
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+        </MapContainer>
     )
 }
