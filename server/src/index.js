3b32f9cca0c5b4de8931d02de63c935e74e0f8a9
jasonpellegrini <jpellegrini1616@gmail.com>
2023-07-11 16:12:23 -0400

    Added dotenv for server

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv';
 
 import { userRouter } from './routes/users.js'
 import { apartmentsRouter } from './routes/apartments.js'
+
+dotenv.config({ path: '../server/src/.env' });
+console.log(process.env.DB_PASSWORD)
 
 const app = express();
 
@@ -11,8 +15,10 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/auth", userRouter);
-
 app.use("/apartments", apartmentsRouter);
 
-mongoose.connect("mongodb+srv://jpellegrini1616:MERN@apartments-oakland-db.9rlokml.mongodb.net/apartments-oakland-db?retryWrites=true&w=majority");
+const password = process.env.DB_PASSWORD;
+const user = process.env.DB_USERNAME;
+
+mongoose.connect(`mongodb+srv://${user}:${password}@apartments-oakland-db.9rlokml.mongodb.net/apartments-oakland-db?retryWrites=true&w=majority`);
 app.listen(3001, () => console.log("server running"))
